406822f9cf78958ac2a6c7d5da57365933ff6be5
ponsonio-aurea <jorge.luis@aurea.com>
2019-01-22 15:31:14 -0500

    componetes + directivas

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-//componets 
 import { AppComponent } from './app.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
-import { ParquesComponent } from './components/parques/parques.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    TiendaComponent,
-    ParquesComponent
+    TiendaComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/components/tienda/tienda.component.ts
+++ my-app/src/app/components/tienda/tienda.component.ts
@@ -1,4 +1,7 @@
+
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
+
 
 @Component( {
     selector: 'tienda-component',
@@ -7,10 +10,42 @@ import { Component } from '@angular/core';
 })
 
 export class TiendaComponent {
-    public titulo;
+    public titulo: string;
+    public productos: Array<String> = ['iPhone1', 'iPhone2', 'iPhone3'];
+    private servidor;
+    public empleado: Empleado;
+    public trabajadores: Array<Empleado>;
+    public mostrarEmpleado = false;
+    public color: string;
+    public color_seleccionado: string;
+
+
+
 
     constructor() {
+        this.color = 'red';
         this.titulo = 'Bienvenidos a mi tienda';
+        this.color_seleccionado = '#ccc';
+        console.log(this.productos);
     }
 
+    ngOnInit() {
+        console.log("ngOnInit");
+        this.empleado = new Empleado('jorge', 41);
+        console.log(this.empleado);
+        this.trabajadores = [
+            new Empleado('Jorge', 33),
+            new Empleado('Lina', 34),
+            new Empleado('Carmen', 35),
+        ];
+        console.log(this.trabajadores);
+    }
+
+    mostrarDatosEmpleado(valor: boolean) {
+        this.mostrarEmpleado = valor;
+    }
+
+    log(mensaje: string) {
+        console.log('log -->' + mensaje);
+    }
 }
